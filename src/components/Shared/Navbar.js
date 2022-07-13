12bc76b40f1b0092a6d4c997ba2917ed377297ac
arfat132 <arfatbegum90@gmail.com>
2022-07-13 18:02:38 +0300

    navbar implemented

---
+++ src/components/Shared/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav class="navbar navbar-expand-lg px-5 shadow-sm">
+            <div class="container-fluid">
+                <a class="navbar-brand fs-3 fw-bold logo" href="#">FakeShop</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav px-5">
+                        <a class="nav-link active fw-semibold text-dark text-Capitalize" aria-current="page" href="#">Home</a>
+                        <a class="nav-link fw-semibold text-dark text-Capitalize" href="#">Shop</a>
+                        <a class="nav-link fw-semibold text-dark text-Capitalize" href="#">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
