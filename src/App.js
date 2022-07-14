f190dd034d0cde2a849fdd02c7c4cbfff33c12e3
arfat132 <arfatbegum90@gmail.com>
2022-07-14 17:11:01 +0300

    carousel implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Home from './components/Home';
 import Navbar from './components/Shared/Navbar';
 
 function App() {
   return (
     <div className="App">
-     <Navbar></Navbar>
+      <Navbar></Navbar>
+      <Home></Home>
     </div>
   );
 }
--- src/components/Shared/Navbar.js
+++ src/components/Shared/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-expand-lg px-5 shadow-sm">
+        <nav class="navbar navbar-expand-lg px-5 shadow-sm py-3 header">
             <div class="container-fluid">
                 <a class="navbar-brand fs-3 fw-bold logo" href="#">FakeShop</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,9 +10,9 @@ const Navbar = () => {
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                     <div class="navbar-nav px-5">
-                        <a class="nav-link active fw-semibold text-dark text-Capitalize" aria-current="page" href="#">Home</a>
-                        <a class="nav-link fw-semibold text-dark text-Capitalize" href="#">Shop</a>
-                        <a class="nav-link fw-semibold text-dark text-Capitalize" href="#">Contact</a>
+                        <a class="nav-link active fw-semibold text-light text-Capitalize" aria-current="page" href="#">Home</a>
+                        <a class="nav-link fw-semibold text-light text-Capitalize" href="#">Shop</a>
+                        <a class="nav-link fw-semibold text-light text-Capitalize" href="#">Contact</a>
                     </div>
                 </div>
             </div>
